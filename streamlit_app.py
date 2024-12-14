e8e65dabcc6d5b336f6298302afa2cbcb088cfbe
Precious-Anucha <preciousanucha2000@gmail.com>
2024-12-14 08:31:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Breast Cancer Detection App')
 
 st.info('This application is used to detect breast Cancer!')
+
+df = pd.read_csv('https://github.com/Jean-njoroge/Breast-cancer-risk-prediction/blob/master/data/clean-data.csv')
+df.head()
