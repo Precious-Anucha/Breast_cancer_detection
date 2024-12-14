6547cf5548070930a552b53e2f44c26140e9d78c
Precious-Anucha <preciousanucha2000@gmail.com>
2024-12-14 08:42:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,6 @@ st.title('🎈 Breast Cancer Detection App')
 
 st.info('This application is used to detect breast Cancer!')
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
-names = ['id', 'clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
-       'marginal_adhesion', 'single_epithelial_size', 'bare_nuclei',
-       'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
-df = pd.read_csv(url, names=names)
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
