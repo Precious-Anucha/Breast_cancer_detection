39bcd91d875a7a44d6ea9859faa5dc80e4c5bd23
Precious-Anucha <preciousanucha2000@gmail.com>
2024-12-14 20:59:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,12 @@ st.info('This application is used to detect breast Cancer!')
 
 
 
-df = pd.read_csv('https://github.com/Precious-Anucha/Breast_cancer_detection/blob/master/breast-cancer-wisconsin.data')
-df
+#df = pd.read_csv('https://github.com/Precious-Anucha/Breast_cancer_detection/blob/master/breast-cancer-wisconsin.data')
+#df
+
+url = 'https://raw.githubusercontent.com/Precious-Anucha/Breast_cancer_detection/main/breast-cancer-wisconsin.data'
+names = ['id', 'clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
+       'marginal_adhesion', 'single_epithelial_size', 'bare_nuclei',
+       'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
+df = pd.read_csv(url, names=names, on_bad_lines='skip')
+print(df.head())
