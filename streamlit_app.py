256f5e271412d294eaed0d3c74be5a3967c32ff3
Precious-Anucha <preciousanucha2000@gmail.com>
2024-12-14 21:26:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,15 @@ st.info('This application is used to detect breast Cancer!')
 
 #url = 'https://github.com/Precious-Anucha/Breast_cancer_detection/blob/master/wdbc.data'
 
-url = 'https://github.com/Precious-Anucha/Breast_cancer_detection/blob/master/breast-cancer-wisconsin.data'
+#url = 'https://github.com/Precious-Anucha/Breast_cancer_detection/blob/master/breast-cancer-wisconsin.data'
+#names = ['id', 'clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
+       'marginal_adhesion', 'single_epithelial_size', 'bare_nuclei',
+       'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
 names = ['id', 'clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
        'marginal_adhesion', 'single_epithelial_size', 'bare_nuclei',
        'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
+#df = pd.read_csv(url, names=names)
 try:
     df = pd.read_csv(url, names=names, on_bad_lines='skip')
     df
