8b1d135e00ac15e17cd9b0e528a34cea4ce97658
Precious-Anucha <preciousanucha2000@gmail.com>
2024-12-14 21:05:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,17 @@ st.info('This application is used to detect breast Cancer!')
 #df = pd.read_csv('https://github.com/Precious-Anucha/Breast_cancer_detection/blob/master/breast-cancer-wisconsin.data')
 #df
 
+
+#url = 'https://raw.githubusercontent.com/Precious-Anucha/Breast_cancer_detection/main/breast-cancer-wisconsin.data'
+
 url = 'https://raw.githubusercontent.com/Precious-Anucha/Breast_cancer_detection/main/breast-cancer-wisconsin.data'
 names = ['id', 'clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
        'marginal_adhesion', 'single_epithelial_size', 'bare_nuclei',
        'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
-df = pd.read_csv(url, names=names, on_bad_lines='skip')
-print(df.head())
+try:
+    df = pd.read_csv(url, header=None, on_bad_lines='skip')
+    #df = pd.read_csv(url, names=names, on_bad_lines='skip')
+    print(df.head())
+except Exception as e:
+    print(f"Error: {e}")
+    print("Check the URL or file location and try again.")
